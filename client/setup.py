bc0f88515b17b23647f7eac26e2d8131b661d926
Бочарников Андрей Сергеевич <@abocharnikov>
2023-02-05 16:45:55 +0300

    Шаблон репозитория

---
+++ client/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='rabbitmq_client',
+    version='1.0.0',
+    author='R&EC SPb ETU',
+    author_email='info@nicetu.spb.ru',
+    url='http://nicetu.spb.ru',
+    description='Работа с брокером сообщений, клиентская часть',
+    long_description="",
+    zip_safe=False,
+    packages=['rabbitmq_client'],
+)
---
+++ server/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='rabbitmq_server',
+    version='1.0.0',
+    author='R&EC SPb ETU',
+    author_email='info@nicetu.spb.ru',
+    url='http://nicetu.spb.ru',
+    description='Работа с брокером сообщений, серверная часть',
+    long_description="",
+    zip_safe=False,
+    packages=['rabbitmq_server'],
+)
